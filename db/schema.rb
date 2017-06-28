3c4e31f98050b44101f041989ab753497365f408
anaalta <suciuanamaria@yahoo.com>
2017-06-28 16:40:07 +0200

    added a salary graph that does not work yet

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170628111746) do
+ActiveRecord::Schema.define(version: 20170628131845) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,16 +52,9 @@ ActiveRecord::Schema.define(version: 20170628111746) do
   end
 
   create_table "salaries", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.float "latitude"
-    t.float "longitude"
   end
 
   add_foreign_key "locations", "responses"
